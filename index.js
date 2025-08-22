8d097d2496ed9fd2e330307092aa0c9c0e02771d
isdera <sangwoog@gmail.com>
2025-08-22 14:52:27 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
+// User 모델 사용
+const {User} = require("./models/User");
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://sangwoog:!jungsw71@sangwoog.vajcajo.mongodb.net/?retryWrites=true&w=majority&appName=sangwoog')
+mongoose.connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
+// 라우터
 app.get('/', (req, res) => {
-  res.send('Hello World!~~ 안녕하세요.')
+  res.send('Hello World!~~ 안녕하세요. 반갑습니다.')
+})
+
+app.post('/register', (req, res) => {
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어준다.
+  
+  const user = new User(req.body);
+
+  user.save()
+    .then(result => {
+      res.status(200).json({
+        success: true        
+      });
+    })
+    .catch(err => {
+      return res.json({
+        success: false, err
+      })
+    })
+/*
+  user.save((err, userInfo) => {
+    if (err) 
+      return res.json({
+        success: false, err
+      })
+    return res.status(200).json({
+        success: true
+    })
+  });
+*/  
 })
 
 app.listen(port, () => {
